b584ee3eb406413e7c10bd2bc7c69a179034f655
Sani1308 <cunanansonnyboy7@gmail.com>
2025-03-29 16:04:00 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+
+const input = document.getElementById("input");
+const addbtn = document.getElementById("add");
+const list = document.getElementById("list");
+
+
+
+addbtn.addEventListener("click", () => {
+    let inputValue = input.value.trim(); 
+    if (inputValue === "") return;
+    
+    let taskItem = document.createElement("div");
+    taskItem.innerHTML = `
+                                        <div class="flex items-center justify-between">
+                            <li class="lg:text-2xl">${inputValue}</li>
+                            <div class="flex gap-3">
+                                <button class="border-[1px] border-none rounded-[5px] bg-blue-400 text-[10px] p-1.5 lg:text-[15px] text-white cursor-pointer hover:bg-blue-600">EDIT</button>
+                                <button class="border-[1px] border-none rounded-[5px] bg-red-500 text-[10px] p-1.5 lg:text-[15px] text-white cursor-pointer hover:bg-red-700">DELETE</button>
+                            </div>
+                        </div>
+                        <div class="w-[280px] h-[1px] bg-gray-300 mt-3 lg:w-[1100px] mb-3"></div>
+`;
+    list.appendChild(taskItem);
+    input.value = "";
+    alert("Added to list!");
+})
+
+const { createElement } = require("lucide");
